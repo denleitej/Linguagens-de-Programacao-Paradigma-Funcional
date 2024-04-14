a7c76fbc98b96b8b0bab8199b2144f30be429898
Dennys Leite Julião <126157075+denleitej@users.noreply.github.com>
2024-04-14 19:59:35 -0300

    index.js

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-const numeros = [1, 2, 3, 4, 5];
+const numeros = [1, 2, 3, 4, 5]; // Criando um array de números
 
-const numPares = numeros.filter(numeros => numeros % 2 === 0);
+const numPares = numeros.filter(numeros => numeros % 2 === 0); // Filtrando os números pares do array utilizando o método filter com callback
 
-console.log(numPares); 
+console.log(numPares); //Para imprimir os números pares em um console de JavaScript real.
+
+// print(numPares); Imprimindo os números pares no console online
