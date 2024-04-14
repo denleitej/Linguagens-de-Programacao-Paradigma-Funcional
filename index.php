925a3258f1eb60822c2ba5bc00f5824ad7cf471a
Dennys Leite Julião <126157075+denleitej@users.noreply.github.com>
2024-04-14 20:00:01 -0300

    index.php

--- index.php
+++ index.php
@@ -1,10 +1,8 @@
 <?php
+$numeros = [1, 2, 3, 4, 5]; // Criando um array de números
 
-$numeros = [1, 2, 3, 4, 5];
-
+// Usando a função array_filter para filtrar os números pares do array
 $numPares = array_filter($numeros, function($numeros) { return $numeros % 2 == 0; });
 
-print_r($numPares);
-
-
+print_r($numPares); // Imprimindo os números pares
 ?>
